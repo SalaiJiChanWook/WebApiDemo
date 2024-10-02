7dc929f3fc773a3e457b688e41762a0f72667342
SalaiChangeWook <uchiasadara6@gmail.com>
2024-10-02 22:52:19 +0630

    feat: :ServiceCollectionExtension

--- WebApiDemo.ApiClient/IoC/ServiceCollectionExtension.cs
+++ WebApiDemo.ApiClient/IoC/ServiceCollectionExtension.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using WebApiDemo.ApiClient.Models;
 
 namespace WebApiDemo.ApiClient.IoC
 {
-    internal class ServiceCollectionExtension
+    public static class ServiceCollectionExtension
     {
+        public static void AddDemoApiClientService(this IServiceCollection services,
+           Action<ApiClientOptions> options )
+        {
+            services.Configure( options );
+            services.AddSingleton(provider =>
+            {
+                var options = provider.GetRequiredService<IOptions<ApiClientOptions>>().Value;
+                return new DemoApiClientService(options);
+            });
+
+        }
     }
 }
